63bda2b2598de94ec6984e04d5d6560c49ec8550
jensnyder <jlsnyder09@gmail.com>
2018-11-02 16:19:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function findMinAndRemoveSorted(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
 
+function merge(firstArray, secondArray) {
+  let mergedArray = [];
+  while (firstArray.length > 0 && secondArray.length > 0) {
+    let firstMin = firstArray[0];
+    let secondMin = secondArray[0];
+    if (firstMin < secondMin) {
+      mergedArray.push(firstArray.shift());
+    } else {
+      mergedArray.push(secondArray.shift());
+    }
+  }
+  return mergedArray.concat(firstArray).concat(secondArray);
+}
+
+function mergeSort(array) {
+  let midpoint = array.length / 2;
+  let firstHalf = array.slice(0, midpoint);
+  let secondHalf = array.slice(midpoint, array.length);
+  if (array.length > 1) {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  } else {
+    return array;
+  }
+}
